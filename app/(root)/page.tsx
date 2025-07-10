2ea70b06fd02e72301bddb0ffa7a9c770b541eb9
Nat Silprasert <natsilprasert@gmail.com>
2025-07-10 22:52:21 +0700

    Implement Home Page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,21 @@
+import BookList from '@/components/BookList'
+import BookOverview from '@/components/BookOverview'
+import { sampleBooks } from '@/components/constants'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </div>
+  )
+}
+
+export default Home
